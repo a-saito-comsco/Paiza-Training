daa0f5c0afcb4cc94e066a88b81f6ba638d3bc65
m-matsumoto <m-matsumoto@comsco.co.jp>
2025-08-05 14:10:33 +0900

    D-class-3 commit

---
+++ paiza-training/D214/solution_1.cs
@@ -0,0 +1,13 @@
+using System;
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        var heightA = int.Parse(Console.ReadLine());
+        var heightB = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine(heightA-heightB);
+    }
+}
