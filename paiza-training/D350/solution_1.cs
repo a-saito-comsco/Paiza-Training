bcafb9ddb4fde89eb50804b7503f036f3d7c6139
m-matsumoto <m-matsumoto@comsco.co.jp>
2025-08-05 12:41:28 +0900

    first commit

---
+++ paiza-training/D350/solution_1.cs
@@ -0,0 +1,13 @@
+using System;
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        var line1 = int.Parse(Console.ReadLine());
+        var line2 = int.Parse(Console.ReadLine());
+        var line=line1*line2;
+        Console.WriteLine(line);
+    }
+}       
