1c1d0160c8d5abc8e0df04f38e208d42fd005cab
m-matsumoto <m-matsumoto@comsco.co.jp>
2025-08-07 11:52:09 +0900

    B-class-5 second commit

--- paiza-training/skill_check/B045/solution_2.cs
+++ paiza-training/skill_check/B045/solution_2.cs
@@ -1,65 +1,74 @@
-    using System;
-    using Linq;
-    class Program
+using System;
+using System.Linq;
+class Program
+{
+    static void Main()
     {
-        static void Main()
-        {
-            var line = Console.ReadLine();
-            string[] split = line.Split(" ");
-            int setA_amt = int.Parse(split[0]);
-            int setB_amt = int.Parse(split[1]);
-            string[] setA = new string[setA_amt];
-            string[] setB = new string[setB_amt];
-            
-            int i = 0;
-            int cnt = 0;
-            var random = new Random();
-            
-            //a1 + a2 =
-            while (StCount(setA) < setA_amt){
-                int a1 = 50;
-                int a2 = 50;
-                while(a1 + a2 >= 100){
-                    a1 = random.Next(0,100);
-                    a2 = random.Next(0,100);
-                }
-                for (i = 0; i < StCount(setA); i++) {
-                    if ((a1.ToString() + " + " + a2.ToString() + " =") == setA[i]) {
-                        break;
-                    }
-                }
-                if (i == StCount(setA)){
-                    setA[i] = (a1.ToString() + " + " + a2.ToString() + " =");
-                }
-            }
-            for (i = 0; i < setA_amt ; i++) {
-                Console.WriteLine(setA[i]);
-            }
-
-            //b1 - b2 =
-            while (StCount(setB) < setB_amt) {
-                int b1 = 0;
-                int b2 = 1;
-                while (b1 < b2){
-                    b1 = random.Next(0,100);
-                    b2 = random.Next(0,100);
-                }
-                for (i=0; i < StCount(setB); i++){
-                    if ((b1.ToString() + " - " + b2.ToString() + " =") == setB[i]) {
-                        break;
-                    }
-                }
-                if (i == StCount(setB)) {
-                    setB[i] = (b1.ToString() + " - " + b2.ToString() + " =");
-                }
-            }
-            for (i = 0; i < setB_amt ; i++) {
-                Console.WriteLine(setB[i]);
-            }
-            Console.WriteLine("");
-        }
+        var line = Console.ReadLine();
+        string[] split = line.Split(" ");
+        int setA_amt = int.Parse(split[0]);
+        int setB_amt = int.Parse(split[1]);
+        Datum[] add = new Datum[10000];
+        Datum[] sub = new Datum[10000];
         
-        static int StCount(string[] st) {
-            return (st != null) => cnt;
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        for(i = 0; i < 100; i++) {
+            for (j = 0; j < 100; j++) {
+                add[k] = new Datum();
+                add[k].st = i.ToString() + " + " + j.ToString() + " =";
+                add[k].pre_cnt = i;
+                add[k].post_cnt = j;
+                add[k].done = 0;
+                sub[k] = new Datum();
+                sub[k].st = i.ToString() + " - " + j.ToString() + " =";
+                sub[k].pre_cnt = i;
+                sub[k].post_cnt = j;
+                sub[k].done = 0;
+                k = k + 1;
+            }
+        }   
+        var random = new Random();
+        
+        //a1 + a2 =
+        int r = random.Next(0, 10000);
+        for (i = 0; i < setA_amt ; i++) {                  
+            while (add[r].done == 1 || (add[r].pre_cnt + add[r].post_cnt >= 100)) {
+                r = random.Next(0, 10000);
+            }
+            add[r].done = 1;
+            Console.WriteLine(add[r].st);
+        }
+
+        //b1 - b2 =
+        r = random.Next(0, 10000);
+        for (i = 0; i < setB_amt ; i++) {                  
+            while (sub[r].done == 1 || (sub[r].pre_cnt < sub[r].post_cnt)) {
+                r = random.Next(0, 10000);
+            }
+            sub[r].done = 1;
+            Console.WriteLine(sub[r].st);
         }
     }
+
+    public class Datum {
+        public string st;
+        public int pre_cnt;
+        public int post_cnt;
+        public int done;
+    }   
+    
+    /*static int StCount(string[] st) {
+        //return st.Count(s => s != null);
+        int i=0;
+        int cnt=0;
+        while(i < st.Length){
+            if(st[i]!=null){
+                cnt += 1;
+            }
+            i+=1;
+        }
+        return cnt;
+    }*/
+}
